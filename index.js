39868776da37986e08a6339c19ea5f317be88ac3
Jcastle52 <castillojasiel11@gmail.com>
2025-05-29 20:37:34 -0500

    clean

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 let code1;
 code1 = 7 + 3;
-
 let code2 = 88 / 2 - 4;
-
 let code3 = 139 % 100;
-
 let message = "The vault has been secured. The combination is:";
-
 let codeA = code1 + "-" + code2 + "-" + code3;
-
 let codeB = `${code1}-${code2}-${code3}`
+
 console.log(message, codeA, codeB)
